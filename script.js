bce235d9454dff2b08592c81614cabba78999775
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-06-07 14:45:12 -0300

    Write playRound function

--- script.js
+++ script.js
@@ -5,20 +5,49 @@ function getComputerChoice() {
 
     switch(randomNum) {
         case 0:
-            choice = "Rock";
+            choice = "rock";
             break;
         case 1:
-            choice = "Paper";
+            choice = "paper";
             break;
         case 2:
-            choice = "Scissors";
+            choice = "scissors";
             break;
     }
 
-    console.log(choice);
     return choice;
 }
 getComputerChoice();
 
-// Ask for the player`s choice and store it in a variable
-// Write logic to determine the winner or if it`s a draw and display it to the player
+function playRound(playerChoice, computerChoice) {
+    switch(playerChoice.toLowerCase()) {
+        case "rock":
+            if(computerChoice === "rock") {
+                return "It`s a Draw";
+            } else if(computerChoice === "paper") {
+                return "You Lose! Paper beats Rock";
+            } else {
+                return "You Win! Rock beats Scissors";
+            }
+        case "paper":
+            if(computerChoice === "rock") {
+                return "You Win! Paper beats Rock";
+            } else if(computerChoice === "paper") {
+                return "It`s a Draw";
+            } else {
+                return "You Lose! Rock beats Paper";
+            }
+        case "scissors":
+            if(computerChoice === "rock") {
+                return "You Lose! Rock beats Scissors";
+            } else if(computerChoice === "paper") {
+                return "You Win! Scissors beats Paper";
+            } else {
+                return "It`s a Draw";
+            }
+    }
+}
+
+let playerChoice = getComputerChoice();
+let computerChoice = getComputerChoice();
+console.log(`player: ${playerChoice} computer: ${computerChoice} result: ${playRound(playerChoice, computerChoice)}`);
